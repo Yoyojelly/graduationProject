24c62dda8344cf76827f90c2aaf7972ee2385b39
Fan <fan@localhost.localdomain>
2019-03-06 12:47:52 +0800

    配置路由

--- WebPages/src/App.vue
+++ WebPages/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
+    <p>
+      <router-link></router-link>
+    </p>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+
   export default {
     name: 'app',
     components: {
-      HelloWorld
     }
   }
 </script>
---
+++ WebPages/src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="LoginDiv">
+        这里是登陆页面1
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    #LoginDiv {
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        margin-left: -0.6%;
+        margin-top: -1.2%;
+    }
+</style>
